cb57b76238b2918939e972f868b4f5a96d97a668
Koray Liman <69529444+KorayLiman@users.noreply.github.com>
2022-01-22 17:56:56 +0300

    qewqw

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Hello",
+      theme: ThemeData(primarySwatch: Colors.teal),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Title"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Press Count",
+              style: TextStyle(fontSize: 36),
+            ),
+            Text("0",
+                style: TextStyle(fontSize: 42, fontWeight: FontWeight.bold)),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
